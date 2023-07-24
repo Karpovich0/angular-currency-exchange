b2b486a497836ee2fb3ce8c5f5b9104764046bd4
Olekka <noodarius0@gmail.com>
2023-07-24 10:57:44 +0300

    finish inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-currency-exchange';
+export class AppComponent implements OnInit {
+  currency1: string = '';
+  currency2: string = '';
+  result: number = 0;
+
+  // Calculate(val1: string, val2: string) {
+  //   this.result = +val1 + +val2;
+  // }
+  Total() {
+    this.result = +this.currency1 + +this.currency2;
+  }
+  constructor() {}
+  ngOnInit(): void {}
 }
